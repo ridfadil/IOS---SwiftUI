ad1f4c55961a47151671222c12d613936c3906c0
Muhamad Farid Padilah <bbmfrd@gmail.com>
2020-07-24 07:31:22 +0700

    Initial Commit

---
+++ SwiftBaru/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftBaru
+//
+//  Created by Muhamad Farid Padilah on 24/07/20.
+//  Copyright © 2020 Latihan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
